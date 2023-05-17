8c0d47930daafc91517566762a81e50995cc5473
Abi (She/her) <91515654+awhittle2@users.noreply.github.com>
2023-05-17 15:19:04 -0700

    Test

--- test/cookbooks/test/recipes/default.rb
+++ test/cookbooks/test/recipes/default.rb
@@ -4,3 +4,32 @@ php_install 'php' do
 end
 
 apt_update 'update'
+
+# Create a test pool
+php_fpm_pool 'test-pool' do
+  action :install
+end
+
+# Add PEAR channel
+php_pear_channel 'pear.php.net' do
+  binary php_pear_path
+  action :update
+end
+
+# Install https://pear.php.net/package/HTTP2
+php_pear 'HTTP2' do
+  binary php_pear_path
+end
+
+# Add PECL channel
+php_pear_channel 'pecl.php.net' do
+  binary php_pear_path
+  action :update
+end
+
+# Install https://pecl.php.net/package/sync
+php_pear 'sync-binary' do
+  package_name 'sync'
+  binary 'pecl'
+  priority '50'
+end
